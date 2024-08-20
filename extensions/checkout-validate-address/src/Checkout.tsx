f5c00d2380e0b6a6f4a280d34726a37582594d43
xuanhanh.dev <xuanhanh.setdy@gmail.com>
2024-08-20 14:40:37 +0700

    update checkout validate address

--- extensions/checkout-validate-address/src/Checkout.tsx
+++ extensions/checkout-validate-address/src/Checkout.tsx
@@ -15,17 +15,27 @@ function App() {
 
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
     if (canBlockProgress) {
-      if (
-        (address.address1 && address.address1.length > 40) ||
-        (address.address2 && address.address2.length > 40)
-      ) {
+      if (address.address1 && address.address1.length > 40) {
         return {
           behavior: "block",
           reason: "Invalid shipping address",
           errors: [
             {
-              // An error without a `target` property is shown at page level
               message: "Please keep address to max. 40 characters",
+              // Show an error underneath the country code field
+              target: "$.cart.deliveryGroups[0].deliveryAddress.address1",
+            },
+          ],
+        };
+      } else if (address.address2 && address.address2.length > 40) {
+        return {
+          behavior: "block",
+          reason: "Invalid shipping address",
+          errors: [
+            {
+              message: "Please keep address to max. 40 characters",
+              // Show an error underneath the country code field
+              target: "$.cart.deliveryGroups[0].deliveryAddress.address2",
             },
           ],
         };
